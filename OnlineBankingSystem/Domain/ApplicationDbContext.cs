00a67140e4a31aadabe2accafd3e110bf29f6bad
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-02-11 14:02:48 +0600

    Add migration for seeding default admin user and roles data

--- OnlineBankingSystem/Domain/ApplicationDbContext.cs
+++ OnlineBankingSystem/Domain/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OnlineBankingSystem.Entities;
 
@@ -7,5 +8,62 @@ namespace OnlineBankingSystem.Domain
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedDefaultAdminUser(builder);
+            SeedRoles(builder);
+            SeedDefaultAdminUserRole(builder);
+        }
+
+        private void SeedDefaultAdminUser(ModelBuilder builder)
+        {
+            var user = new ApplicationUser
+            {
+                Id = "b74ddd14-6340-4840-95c2-db12554843e5",
+                FirstName = "Default",
+                LastName = "Admin",
+                UserName = "admin",
+                NormalizedUserName = "admin".ToUpper(),
+                Email = "admin@test.com",
+                NormalizedEmail = "admin@test.com".ToUpper(),
+                LockoutEnabled = false,
+            };
+
+            var passwordHasher = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHasher.HashPassword(user, "admin123");
+
+            builder.Entity<ApplicationUser>().HasData(user);
+        }
+
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "fab4fac1-c546-41de-aebc-a14da6895711",
+                    Name = Enums.Roles.Admin.ToString(),
+                    ConcurrencyStamp = "1",
+                    NormalizedName = Enums.Roles.Admin.ToString()
+                },
+                new IdentityRole
+                {
+                    Id = "c7b013f0-5201-4317-abd8-c211f91b7330",
+                    Name = Enums.Roles.User.ToString(),
+                    ConcurrencyStamp = "2",
+                    NormalizedName = Enums.Roles.User.ToString()
+                });
+        }
+
+        private void SeedDefaultAdminUserRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "b74ddd14-6340-4840-95c2-db12554843e5",
+                    RoleId = "fab4fac1-c546-41de-aebc-a14da6895711"
+                });
+        }
     }
 }
---
+++ OnlineBankingSystem/Migrations/20230211080058_SeedDefaultAdminAndRoles.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OnlineBankingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultAdminAndRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "c7b013f0-5201-4317-abd8-c211f91b7330", "2", "User", "User" },
+                    { "fab4fac1-c546-41de-aebc-a14da6895711", "1", "Admin", "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "BirthDate", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "b74ddd14-6340-4840-95c2-db12554843e5", 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "3e2d16d5-0caa-4e10-8a28-cbc03d2e8ec0", "admin@test.com", false, "Default", "Admin", false, null, "ADMIN@TEST.COM", "ADMIN", "AQAAAAEAACcQAAAAEOPnbVb6jYz+zR5BWHxJtlKz3xH5i0E5ciEfuesuTnHWNAoFf0cxCefmA5pn/o79bw==", null, false, "dfddf685-ee7a-4803-a1e9-99f5d4f16e52", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "fab4fac1-c546-41de-aebc-a14da6895711", "b74ddd14-6340-4840-95c2-db12554843e5" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7b013f0-5201-4317-abd8-c211f91b7330");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "fab4fac1-c546-41de-aebc-a14da6895711", "b74ddd14-6340-4840-95c2-db12554843e5" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fab4fac1-c546-41de-aebc-a14da6895711");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5");
+        }
+    }
+}
