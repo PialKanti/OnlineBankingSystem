8252f4e06081aa8cb3de1f0c715c08272b72f60e
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-02-17 14:15:46 +0600

    Registered group of services in extension methods

---
+++ OnlineBankingSystem/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using OnlineBankingSystem.Entities;
+using OnlineBankingSystem.Options;
+using OnlineBankingSystem.Repositories;
+using OnlineBankingSystem.Services;
+
+namespace OnlineBankingSystem.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuthServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ClockSkew = TimeSpan.Zero,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["JwtToken:ValidIssuer"],
+                        ValidAudience = configuration["JwtToken:ValidAudience"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(configuration["JwtToken:Key"])) //Todo keep IssuerSigningKey in .env file or user secrets
+                    });
+
+            services.AddAuthorization();
+
+            return services;
+        }
+
+        public static IServiceCollection AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                //User Settings
+                options.User.RequireUniqueEmail = true;
+
+                //Password Settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
+
+            services.Configure<JwtTokenOptions>(configuration.GetSection(JwtTokenOptions.JwtToken));
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUsersRepository<ApplicationUser>, UsersRepository>();
+            services.AddScoped<JwtTokenService, JwtTokenService>();
+
+            return services;
+        }
+    }
+}
--- OnlineBankingSystem/Program.cs
+++ OnlineBankingSystem/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.OpenApi.Models;
 using OnlineBankingSystem.Domain;
 using OnlineBankingSystem.Dtos;
 using OnlineBankingSystem.Entities;
+using OnlineBankingSystem.Extensions.DependencyInjection;
 using OnlineBankingSystem.Options;
 using OnlineBankingSystem.Repositories;
 using OnlineBankingSystem.Services;
@@ -42,22 +43,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    //User Settings
-    options.User.RequireUniqueEmail = true;
-
-    //Password Settings
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-});
-
-builder.Services.AddScoped<IUsersRepository<ApplicationUser>, UsersRepository>();
-builder.Services.AddScoped<JwtTokenService, JwtTokenService>();
+builder.Services.AddConfigurationOptions(builder.Configuration);
 
 var mapperConfig = new MapperConfiguration(configuration =>
 {
@@ -67,37 +53,9 @@ var mapperConfig = new MapperConfiguration(configuration =>
 IMapper mapper = mapperConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
 
-builder.Services.Configure<RouteOptions>(options =>
-{
-    options.LowercaseUrls = true;
-});
+builder.Services.AddAuthServices(builder.Configuration);
 
-builder.Services.Configure<JwtTokenOptions>(builder.Configuration.GetSection(JwtTokenOptions.JwtToken));
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = false,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["JwtToken:ValidIssuer"],
-            ValidAudience = builder.Configuration["JwtToken:ValidAudience"],
-            IssuerSigningKey =
-                new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(
-                        builder.Configuration[
-                            "JwtToken:Key"])) //Todo keep IssuerSigningKey in .env file or user secrets
-        });
-
-builder.Services.AddAuthorization();
+builder.Services.AddApplicationServices();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
